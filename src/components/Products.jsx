9d09186f32cba1015d3b080ef133d7a22750f041
Sam <sayyamo56@gmail.com>
2026-07-21 13:53:08 +0530

    one line

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -89,7 +89,7 @@ function Products() {
         </h2>
 
         {/* Grid */}
-        <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-8">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-8">
           {products.map((product, index) => (
             <div
               key={index}
