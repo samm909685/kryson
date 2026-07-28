f6802f1c8fcc36a2e3c0dd717ce1477fecff2ee9
Sam <sayyamo56@gmail.com>
2026-07-28 22:57:40 +0530

    database

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,12 +3,6 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-console.log("DB_HOST =", process.env.DB_HOST);
-console.log("DB_USER =", process.env.DB_USER);
-console.log("DB_PASSWORD =", process.env.DB_PASSWORD);
-console.log("DB_NAME =", process.env.DB_NAME);
-console.log("DB_PORT =", process.env.DB_PORT);
-
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
@@ -16,13 +10,5 @@ const pool = mysql.createPool({
   database: process.env.DB_NAME,
   port: process.env.DB_PORT,
 });
-
-try {
-  const connection = await pool.getConnection();
-  console.log("✅ MySQL Connected Successfully");
-  connection.release();
-} catch (err) {
-  console.error("❌ MySQL Connection Error:", err);
-}
 
 export default pool;
