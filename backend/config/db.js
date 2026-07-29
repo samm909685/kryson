c045a33481c8b4dc4828659c6f4bf71afc5dd5cb
Sam <sayyamo56@gmail.com>
2026-07-29 11:31:02 +0530

    database

--- backend/config/db.js
+++ backend/config/db.js
@@ -14,6 +14,9 @@ const pool = mysql.createPool({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: Number(process.env.DB_PORT),
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 (async () => {
