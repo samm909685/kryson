b5368b0cc25ceb15e1ad1073c1e7eadceb92e1d9
Sam <sayyamo56@gmail.com>
2026-07-29 18:59:29 +0530

    frontend

--- backend/server.js
+++ backend/server.js
@@ -4,10 +4,14 @@ import dotenv from "dotenv";
 import pool from "./config/db.js";
 import adminRoutes from "./routes/adminRoutes.js";
 import medicineRoutes from "./routes/medicineRoutes.js";
+import path from "path";
+import { fileURLToPath } from "url";
 
 dotenv.config();
 
 const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.use(cors());
 app.use(express.json());
@@ -31,7 +35,12 @@ app.get("/api/health", async (req, res) => {
 // Admin Routes
 app.use("/api/admin", adminRoutes);
 app.use("/api/medicines", medicineRoutes);
+app.use(express.static(path.join(__dirname, "../dist")));
 
+// Serve React app for all non-API routes
+app.get(/^\/(?!api).*/, (req, res) => {
+  res.sendFile(path.join(__dirname, "../dist/index.html"));
+});
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
