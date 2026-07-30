810a556c8d32359ad15ec5e7dac6de27ee3dc3fb
Sam <sayyamo56@gmail.com>
2026-07-30 20:37:09 +0530

    logoout

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { HashRouter, Routes, Route } from "react-router-dom";
 import Home from "./Home";
 import AdminLogin from "./components/AdminLogin";
+import ProtectedRoute from "./components/ProtectedRoute";
+
 import AdminDashboard from "./pages/AdminDashboard";
 import AddMedicine from "./pages/AddMedicine";
 import MedicineList from "./pages/MedicineList";
@@ -15,14 +17,55 @@ function App() {
         {/* Portfolio */}
         <Route path="/" element={<Home />} />
 
-        {/* Admin Login */}
+        {/* Public Routes */}
         <Route path="/admin" element={<AdminLogin />} />
-        <Route path="/admin/dashboard" element={<AdminDashboard />} />
-        <Route path="/admin/add-medicine" element={<AddMedicine />} />
-        <Route path="/admin/medicines" element={<MedicineList />} />
         <Route path="/verify/:token" element={<VerifyMedicine />} />
-        <Route path="/admin/scan-history" element={<ScanHistory />} />
-        <Route path="/admin/settings" element={<Settings />} />
+
+        {/* Protected Admin Routes */}
+        <Route
+          path="/admin/dashboard"
+          element={
+            <ProtectedRoute>
+              <AdminDashboard />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/admin/add-medicine"
+          element={
+            <ProtectedRoute>
+              <AddMedicine />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/admin/medicines"
+          element={
+            <ProtectedRoute>
+              <MedicineList />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/admin/scan-history"
+          element={
+            <ProtectedRoute>
+              <ScanHistory />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/admin/settings"
+          element={
+            <ProtectedRoute>
+              <Settings />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
     </HashRouter>
   );
